6fc0fcc5e6be670665a69bda1c93e216145ea302
ashok.se <ashok.se@media.net>
2023-04-10 13:17:15 +0530

    added new unit test

---
+++ app/src/test/java/com/ashok/circleci/SmallTest.kt
@@ -0,0 +1,12 @@
+package com.ashok.circleci
+
+import org.junit.Assert
+import org.junit.Test
+
+class SmallTest {
+
+    @Test
+    fun `checkGivenNumberis5`(){
+        Assert.assertEquals(4, 5)
+    }
+}
